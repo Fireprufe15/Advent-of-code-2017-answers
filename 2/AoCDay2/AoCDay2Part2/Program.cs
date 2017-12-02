cc2cb783948d5b9907a6332dacb295b3d5dafdcf
MG Potgieter <mgandr@gmail.com>
2017-12-02 10:49:33 +0200

    Day 2 Complete

---
+++ 2/AoCDay2/AoCDay2Part2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay2Part2 {
+    class Program {
+        static void Main(string[] args) {
+
+            int sum = 0;
+            char delimiter = '\t';
+            List<List<int>> table = new List<List<int>>();
+
+            using (TextReader tr = new StreamReader(new FileStream("in.txt", FileMode.OpenOrCreate))) {
+                while (tr.Peek() > 0) {
+                    string[] line = tr.ReadLine().Split(delimiter);
+                    int[] line2 = Array.ConvertAll<String, int>(line, int.Parse);
+                    table.Add(line2.ToList());
+                }
+            }
+
+            foreach (var item in table) {
+
+                foreach (var number in item) {
+                    foreach (var othernumber in item) {
+                        if (number == othernumber) {
+                            continue;
+                        }
+                        if ((number%othernumber) == 0) {
+                            sum += number / othernumber;
+                            break;
+                        }
+                    }
+                }
+                
+
+            }
+
+            Console.WriteLine(sum.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
