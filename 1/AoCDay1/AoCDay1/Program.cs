a9a21e99fd86a75dabaf94a2303ebaab82a0d04d
MG Potgieter <mgandr@gmail.com>
2017-12-02 10:00:54 +0200

    Day 1 Complete

---
+++ 1/AoCDay1/AoCDay1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay1 {
+    class Program {
+        static void Main(string[] args) {
+
+            string input;
+
+            using (TextReader tr = new StreamReader(new FileStream("in.txt", FileMode.OpenOrCreate))) {
+                input = tr.ReadToEnd();
+            }
+
+            List<int> vals = new List<int>();
+
+            for (int i = 0; i < input.Length; i++) {
+                if (i == input.Length-1) {
+                    if (input[i] == input[0]) {
+                        vals.Add(int.Parse(input[i].ToString()));                        
+                    }
+                    continue;
+                }
+                if (input[i] == input[i+1]) {
+                    vals.Add(int.Parse(input[i].ToString()));
+                }
+            }
+
+            var res = vals.Sum();
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 1/AoCDay1/AoCDay1Part2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay1Part2 {
+    class Program {
+        static void Main(string[] args) {
+
+            string input; /*= Console.ReadLine();*/
+
+            using (TextReader tr = new StreamReader(new FileStream("in.txt", FileMode.OpenOrCreate))) {
+                input = tr.ReadToEnd();
+            }
+
+            List<int> vals = new List<int>();
+
+            for (int i = 0; i < input.Length; i++) {
+                if (i >= input.Length/2) {
+                    var loc = input.Length - i;
+                    var looped = (input.Length / 2) - loc;
+                    if (input[looped] == input[i]) {
+                        vals.Add(int.Parse(input[i].ToString()));
+                    }
+                    continue;
+                }
+                if (input[i] == input[i + (input.Length/2)]) {
+                    vals.Add(int.Parse(input[i].ToString()));
+                }
+            }
+
+            var res = vals.Sum();
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+
+    }    
+}
