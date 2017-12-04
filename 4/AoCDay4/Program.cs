e5d5e981baa96e18822ef5e9dbacc6bd6282acfb
MG Potgieter <mgandr@gmail.com>
2017-12-04 09:09:35 +0200

    Day 4 done. LINQ is cheating but I do it anyway

---
+++ 4/AoCDay4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            int validPasswords = 0;
+
+            using (TextReader tr = new StreamReader(new FileStream("input.txt", FileMode.OpenOrCreate)))
+            {
+                while (tr.Peek() > 0)
+                {
+                    input = tr.ReadLine();
+                    var passwords = input.Split(' ');
+                    var invalid = false;
+                    for (int i = 0; i < passwords.Length; i++)
+                    {
+                        for (int j = 0; j < passwords.Length; j++)
+                        {
+                            if (j == i) continue;
+                            if (passwords[i] == passwords[j])
+                            {
+                                invalid = true;
+                                break;
+                            }                            
+                        }
+                        if (invalid) break;
+                    }
+                    if (!invalid) validPasswords++;
+                }
+                Console.WriteLine(validPasswords.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
