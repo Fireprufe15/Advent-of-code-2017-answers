1bc0ea3096de54045e32a6c4a085ec2c5f951c77
MG Potgieter <mgandr@gmail.com>
2017-12-05 09:22:02 +0200

    Day 5 is done

---
+++ 5/AoCDay5/AoCDay5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> commands = new List<int>();
+
+            using (TextReader tr = new StreamReader(new FileStream("input.txt", FileMode.OpenOrCreate)))
+            {
+                while (tr.Peek() > 0)
+                {
+                    commands.Add(int.Parse(tr.ReadLine()));
+                }
+            }
+
+            int stepCounter = 0;
+            var pos = 0;
+            while (pos < commands.Count && pos > -1)
+            {
+                stepCounter++;
+                var newpos = pos + commands[pos];
+                commands[pos]++;
+                pos = newpos;
+            }
+            Console.WriteLine(stepCounter);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5/AoCDay5/AoCDay5Part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay5Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> commands = new List<int>();
+
+            using (TextReader tr = new StreamReader(new FileStream("input.txt", FileMode.OpenOrCreate)))
+            {
+                while (tr.Peek() > 0)
+                {
+                    commands.Add(int.Parse(tr.ReadLine()));
+                }
+            }
+
+            int stepCounter = 0;
+            var pos = 0;
+            while (pos < commands.Count && pos > -1)
+            {
+                stepCounter++;
+                var newpos = pos + commands[pos];
+                if (commands[pos] >= 3)
+                {
+                    commands[pos]--;
+                }
+                else
+                {
+                    commands[pos]++;
+                }
+                pos = newpos;
+            }
+            Console.WriteLine(stepCounter);
+            Console.ReadKey();
+        }
+    }
+}
