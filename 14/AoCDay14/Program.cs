80661fabd2ac68e1b2731e6a006163fb340f6ab5
MG Potgieter <mgandr@gmail.com>
2017-12-17 11:28:20 +0200

    14 done...im on the catch up now

---
+++ 14/AoCDay14/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AoCDay14 {
+    class Program {
+        static void Main(string[] args) {
+
+            KnotHasher.KnotHasher knot = new KnotHasher.KnotHasher();
+            var input = Console.ReadLine();
+            List<string> hashes = new List<string>();
+            List<string> binaryHashes = new List<string>();
+            for (int i = 0; i < 128; i++) {
+                hashes.Add(knot.Hash(input + "-" + i.ToString()));
+            }
+
+            foreach (var item in hashes) {
+                string binarystring = String.Join(String.Empty,
+                    item.Select(
+                    c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+                    )
+                );
+                binaryHashes.Add(binarystring);
+            }
+
+            var blockCount = 0;
+            foreach (var item in binaryHashes) {
+                blockCount += item.Where(x => x == '1').Count();
+            }
+
+            int[,] regionTable = new int[128,128];
+
+            int highestRegion = 0;
+            foreach (var item in binaryHashes) {
+                for (int i = 0; i < item.Length; i++) {
+                    var itemIndex = binaryHashes.IndexOf(item);
+                    if (item[i] == '1' && regionTable[itemIndex,i] == 0) {
+                        highestRegion++;
+                        var existingCoords = new List<Tuple<int, int>>();
+                        try {                            
+                            var regionCoords = TraceRegion(existingCoords, binaryHashes, new Tuple<int, int>(itemIndex, i));                            
+                            foreach (var coord in regionCoords) {
+                                regionTable[coord.Item1, coord.Item2] = highestRegion;
+                            }
+                        }
+                        catch (StackOverflowException e) {
+                            Console.WriteLine(e.Message);                          
+                        }
+                        
+                       
+                    }
+                }
+            }
+
+            Console.WriteLine(blockCount);
+            Console.WriteLine(highestRegion);
+
+            for (int i = 0; i < 128; i++) {
+                for (int j = 0; j < 128; j++) {
+                    Console.Write(regionTable[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<Tuple<int, int>> TraceRegion(List<Tuple<int, int>> existingCoords, List<string> hashes, Tuple<int, int> startCoords) {
+
+            bool exhausted = false;
+            existingCoords.Add(startCoords);
+            while (!exhausted) {
+                exhausted = true;
+                foreach (var item in new List<Tuple<int,int>>(existingCoords)) {
+                    if (item.Item1 > 0 && hashes[item.Item1 - 1][item.Item2] == '1' && !existingCoords.Contains(new Tuple<int, int>(item.Item1 - 1, item.Item2))) {
+                        existingCoords.Add(new Tuple<int, int>(item.Item1 - 1, item.Item2));
+                        exhausted = false;
+                    }
+                    if (item.Item2 > 0 && hashes[item.Item1][item.Item2 - 1] == '1' && !existingCoords.Contains(new Tuple<int, int>(item.Item1, item.Item2 - 1))) {
+                        existingCoords.Add(new Tuple<int, int>(item.Item1, item.Item2 - 1));
+                        exhausted = false;
+                    }
+                    if (item.Item1 < 127 && hashes[item.Item1 + 1][item.Item2] == '1' && !existingCoords.Contains(new Tuple<int, int>(item.Item1 + 1, item.Item2))) {
+                        existingCoords.Add(new Tuple<int, int>(item.Item1 + 1, item.Item2));
+                        exhausted = false;
+                    }
+                    if (item.Item2 < 127 && hashes[item.Item1][item.Item2 + 1] == '1' && !existingCoords.Contains(new Tuple<int, int>(item.Item1, item.Item2 + 1))) {
+                        existingCoords.Add(new Tuple<int, int>(item.Item1, item.Item2 + 1));
+                        exhausted = false;
+                    }
+                }
+            }
+            return existingCoords;
+
+        }
+    }
+}
---
+++ utils/KnotHasher/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnotHasher
+{
+    public class KnotHasher
+    {
+
+        public string Hash(string input) {
+
+            int skipSize = 0;
+            int position = 0;
+            List<int> list = new List<int>(256);
+            List<int> denseHash = new List<int>();
+            List<int> lengths = new List<int>();
+
+            for (int i = 0; i < 256; i++) {
+                list.Add(i);
+            }
+
+            foreach (var item in input) {
+                lengths.Add((int)item);
+            }
+
+            lengths.AddRange(new int[] { 17, 31, 73, 47, 23 });
+
+            for (int i = 0; i < 64; i++) {
+                foreach (var item in lengths) {
+
+                    if (position + item < list.Count) {
+                        var subsequence = list.GetRange(position, item);
+                        subsequence.Reverse();
+                        list.RemoveRange(position, item);
+                        list.InsertRange(position, subsequence);
+                    }
+                    else {
+                        var sub = list.GetRange(position, list.Count - position);
+                        var remainingCount = item - sub.Count;
+                        var sub2 = list.GetRange(0, remainingCount);
+                        sub.AddRange(sub2);
+                        sub.Reverse();
+                        list.RemoveRange(position, list.Count - position);
+                        list.InsertRange(list.Count, sub.Take(sub.Count - sub2.Count));
+                        sub.RemoveRange(0, sub.Count - sub2.Count);
+                        list.RemoveRange(0, sub.Count);
+                        list.InsertRange(0, sub);
+                    }
+
+                    position += item + skipSize;
+                    skipSize++;
+                    while (position >= list.Count) {
+                        position -= list.Count;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 16; i++) {
+                var subList = list.Take(16).ToList();
+                var res = subList[0] ^ subList[1] ^ subList[2] ^ subList[3] ^ subList[4] ^ subList[5] ^ subList[6] ^ subList[7] ^ subList[8] ^ subList[9] ^ subList[10] ^ subList[11] ^ subList[12] ^ subList[13] ^ subList[14] ^ subList[15];
+                denseHash.Add(res);
+                list.RemoveRange(0, 16);
+            }
+
+            string final = "";
+            foreach (var item in denseHash) {
+                var hex = item.ToString("X");
+                if (hex.Length == 1) {
+                    hex = hex.Insert(0, "0");
+                }
+                final += hex;
+            }
+
+            return final;
+
+        }
+
+    }
+}
